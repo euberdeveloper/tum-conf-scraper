fe25eaf89031ccb4b4672a0233f66d0d8713b402
euberdeveloper <euberdeveloper@gmail.com>
2022-09-15 12:18:36 +0200

    refactor: use video-scraper-core
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,170 @@
-// TODO
+![Build](https://github.com/euberdeveloper/tum-conf-scraper/workflows/Build/badge.svg)
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+[![License](https://img.shields.io/npm/l/tum-conf-scraper.svg)](https://github.com/euberdeveloper/tum-conf-scraper/blob/main/LICENSE)
+[![GitHub issues](https://img.shields.io/github/issues/euberdeveloper/tum-conf-scraper.svg)](https://github.com/euberdeveloper/tum-conf-scraper/issues)
+[![GitHub stars](https://img.shields.io/github/stars/euberdeveloper/tum-conf-scraper.svg)](https://github.com/euberdeveloper/tum-conf-scraper/stargazers)
+![npm](https://img.shields.io/npm/v/tum-conf-scraper.svg)
+
+# tum-conf-scraper
+This is a scraper written in Node.js and using Puppeteer that gets the videos served by [Tum Conf](https://tum-conf.zoom.us) services.
+
+## Install
+
+To install tum-conf-scraper, run:
+
+```bash
+$ npm install tum-conf-scraper
+```
+
+## Project purpose
+
+This module is written because videos hosted on [Tum Conf](tum-conf.zoom.us) are difficult to download and watchable only in the browser. By using the module **[video-scraper-core](https://www.npmjs.com/package/video-scraper-core)**, I created this module, that allows those videos to be recorderd.
+
+## Project usage
+
+To scrape a video available at "https://tum-conf.zoom.us/rec/share/myvideo" and save it to "./saved.webm":
+
+```javascript
+const { TumConfScraper } = require('tum-conf-scraper');
+
+async function main() {
+    // Create an instance of the scraper
+    const scraper = new TumConfScraper({
+        debug: true
+    });
+    // Launch the Chrome browser
+    await scraper.launch();
+    // Scrape and save the video
+    await scraper.scrape('mypasscode', 'https://tum-conf.zoom.us/rec/share/myvideo', './saved.webm');
+    // Close the browser
+    await scraper.close();
+}
+main();
+```
+
+To scrape and download more than one video:
+
+```javascript
+const { TumConfScraper } = require('tum-conf-scraper');
+
+async function main() {
+    // Create an instance of the scraper
+    const scraper = new TumConfScraper({
+        debug: true
+    });
+    // Launch the Chrome browser
+    await scraper.launch();
+    // Scrape and save the first video
+    await scraper.scrape('mypasscode', 'https://tum-conf.zoom.us/rec/share/myvideo', './saved.webm');
+    // Scrape and save the second video
+    await scraper.scrape('mypasscode', 'https://tum-conf.zoom.us/rec/share/myvideo-bis', './saved_bis.webm');
+    // Close the browser
+    await scraper.close();
+}
+main();
+```
+
+To scrape and download in parallel more than one video:
+
+```javascript
+const { TumConfScraper } = require('tum-conf-scraper');
+
+async function scrape(dest, link) {
+    // Create an instance of the scraper
+    const scraper = new TumConfScraper({
+        debug: true
+    });
+    // Launch the Chrome browser
+    await scraper.launch();
+    // Scrape and save the video
+    await scraper.scrape('mypasscode', link, dest);
+    // Close the browser
+    await scraper.close();
+}
+
+async function main() {
+    const tasks = [
+        ['./saved.webm', 'https://tum-conf.zoom.us/rec/share/myvideo'],
+        ['./saved_bis.webm', 'https://tum-conf.zoom.us/rec/share/myvideo-bis']
+    ].map(([dest, link]) => scrape(dest, link));
+    await Promise.all(tasks);
+}
+main();
+```
+
+With custom options:
+
+```javascript
+const { TumConfScraper } = require('tum-conf-scraper');
+
+async function main() {
+    // Browser options
+    const scraper = new TumConfScraper({
+        debug: true,
+        debugScope: 'This will be written as scope of the euberlog debug',
+        windowSize: {
+            width: 1000,
+            height: 800
+        },
+        browserExecutablePath: '/usr/bin/firefox'
+    });
+    await scraper.launch();
+
+    // Scraping options
+    await scraper.scrape('mypasscode', 'https://tum-conf.zoom.us/rec/share/myvideo', './saved.webm', { duration: 1000 });
+    await scraper.scrape('mypasscode', 'https://tum-conf.zoom.us/rec/share/myvideo-bis', './saved_bis.webm', { 
+        audio: false,
+        delayAfterVideoStarted: 3000,
+        delayAfterVideoFinished: 2000 
+    });
+
+    await scraper.close();
+}
+main();
+```
+
+...all the options can be seen in the API section or with the Typescript definitions.
+
+## API
+
+The documentation site is: [tum-conf-scraper documentation](https://tum-conf-scraper.euber.dev)
+
+The documentation for development site is: [tum-conf-scraper dev documentation](https://tum-conf-scraper-dev.euber.dev)
+
+### TumConfScraper
+
+The TumConfScraper class, that scrapes a video from a "BBB WebKonferenze" and saves it to a file.
+
+**Syntax:**
+
+`const scraper = new TumConfScraper(passcode, options);`
+
+**Parameters:**
+
+* __passcode__:  A `string` that specifies the passcode to access the video page.
+* __options__: Optional. A `BrowserOptions` object that specifies the options for this instance.
+
+**Methods:**
+
+* __setBrowserOptions(options: BrowserOptions): void__: Changes the browser options with the ones given by the `options` parameter.
+* __launch(): Promise<void>__: Launches the browser window.
+* __close(): Promise<void>__: Closes the browser window.
+* __scrape(url: string, destPath: string, options: ScrapingOptions): Promise<void>__: Scrapes the video in `url` and saves it to `destPath`. Some ScrapingOptions can be passed.
+
+### BrowserOptions
+
+The options given to the TumConfScraper constructor, see [video-scraper-core](https://github.com/euberdeveloper/video-scraper-core#browseroptions) for more information.
+
+### ScrapingOptions
+
+The options passing to a scrape method, see [video-scraper-core](https://github.com/euberdeveloper/video-scraper-core#scrapingoptions) for more information.
+
+### Errors
+
+There are also some error classes that can be thrown by this module, see [video-scraper-core](https://github.com/euberdeveloper/video-scraper-core#errors) for more information.
+
+## Notes
+
+* The default browser is **Google Chrome** on `/usr/bin/google-chrome`, because Chromium did not support the BBB videos. You can always change the browser executable path on the configurations.
+* By default (if the **duration** option is `null`), the **duration of the recording will be automatically detected** by looking at the vjs player of the page and by adding a stopping delay of 15 seconds.
+* This module can be uses only in **headful mode**.
--- source/utils/scraper.ts
+++ source/utils/scraper.ts
@@ -1,25 +1,11 @@
-import * as fs from 'fs';
-import * as puppeteerStream from 'puppeteer-stream';
-// import { Browser } from 'puppeteer';
+import { VideoScraperCore, ScrapingOptions, BrowserOptions } from 'video-scraper-core';
+import { Page } from 'puppeteer';
 import { Logger } from 'euberlog';
-
-import { BrowserOptions, InternalBrowserOptions, ScrapingOptions } from '../types';
-import {
-    TumConfVideoScraperBrowserNotLaunchedError,
-    TumConfVideoScraperDuringBrowserCloseError,
-    TumConfVideoScraperDuringBrowserLaunchError,
-    TumConfVideoScraperDuringScrapingError
-} from '../errors';
-
-import { handleBrowserOptions, handleScrapingOptions } from './options';
 
 /**
  * The [[TumConfVideoScraper]] class, that scrapes a video from a "TumConf WebKonferenze" and saves it to a file.
  */
-export class TumConfVideoScraper {
-    private logger: Logger;
-    private options: InternalBrowserOptions;
-    private browser: /*Browser*/ any | null = null;
+export class TumConfVideoScraper extends VideoScraperCore {
     private readonly passcode: string;
 
     /**
@@ -28,152 +14,31 @@ export class TumConfVideoScraper {
      * @param options The [[BrowserOptions]] to pass to the instance.
      */
     constructor(passcode: string, options: BrowserOptions = {}) {
+        super(options);
         this.passcode = passcode;
-        this.setBrowserOptions(options);
     }
 
-    /**
-     * Given the duration text gotten from the page's HTML (e.g. 1:30:23), it returns the duration in milliseconds.
-     * @param durationText The duration text gotten from the page's HTML.
-     * @returns The duration in milliseconds.
-     */
-    private handleDurationText(durationText: string): number {
-        const [hours, minutes, seconds] = durationText.split(':');
-        return (+hours * 3600 + +minutes * 60 + +seconds) * 1000;
+    protected getFullScreenSelector(): string {
+        return '.vjs-fullscreen-toggle-control-button';
+    }
+    protected getPlayButtonSelector(): string {
+        return '.vjs-big-play-button';
+    }
+    protected getVideoDurationSelector(): string {
+        return '.vjs-remaining-time-display';
     }
 
-    /**
-     * Changes the [[BrowserOptions]] options.
-     * @param options The new options.
-     */
-    public setBrowserOptions(options: BrowserOptions): void {
-        this.options = handleBrowserOptions(options);
-        this.logger = new Logger({
-            debug: this.options.debug,
-            scope: this.options.debugScope
-        });
+    protected async afterPageLoaded(_options: ScrapingOptions, page: Page, logger: Logger): Promise<void> {
+        logger.debug('Putting the passcode to access the video');
+        await page.waitForSelector('input#password');
+        await page.$eval(
+            'input#password',
+            (el: HTMLInputElement, passcode: string) => (el.value = passcode),
+            this.passcode
+        );
 
-        this.logger.debug('BrowserOptions are', this.options);
-    }
-
-    /**
-     * Launches the browser window.
-     */
-    public async launch(): Promise<void> {
-        try {
-            this.logger.debug('Launching browser');
-            this.browser = await puppeteerStream.launch({
-                executablePath: this.options.browserExecutablePath,
-                defaultViewport: null,
-                args: [`--window-size=${this.options.windowSize.width},${this.options.windowSize.height}`]
-            });
-            this.logger.debug('Browser launched');
-        } catch (error) {
-            throw new TumConfVideoScraperDuringBrowserLaunchError(error);
-        }
-    }
-
-    /**
-     * Closes the browser window.
-     */
-    public async close(): Promise<void> {
-        try {
-            if (this.browser) {
-                await this.browser.close();
-            }
-        } catch (error) {
-            throw new TumConfVideoScraperDuringBrowserCloseError(error);
-        }
-    }
-
-    /**
-     * Scrapes a video from a TumConf conference.
-     * @param url The url of the video to save
-     * @param destPath The path where the video will be saved. Note that the extension should be webm.
-     * @param options The [[ScrapingOptions]] to pass to this method.
-     */
-    public async scrape(url: string, destPath: string, options: ScrapingOptions = {}): Promise<void> {
-        const scrapingOptions = handleScrapingOptions(options);
-
-        if (!this.browser) {
-            throw new TumConfVideoScraperBrowserNotLaunchedError();
-        }
-
-        try {
-            const logger = options.useGlobalDebug
-                ? this.logger
-                : new Logger({ debug: scrapingOptions.debug ?? this.options.debug, scope: scrapingOptions.debugScope });
-
-            logger.debug('Launching page and going to the url', url);
-            const page = await this.browser.newPage();
-            await page.goto(url, { waitUntil: 'networkidle0' });
-
-            logger.debug('Putting the passcode to access the video');
-            await page.waitForSelector('input#password');
-            await page.$eval(
-                'input#password',
-                (el: HTMLInputElement, passcode: string) => (el.value = passcode),
-                this.passcode
-            );
-
-            logger.debug('Clicking the button to access the video');
-            await page.waitForSelector('.btn-primary.submit');
-            await page.$eval('.btn-primary.submit', (button: HTMLButtonElement) => button.click());
-
-            if (scrapingOptions.duration === null) {
-                logger.debug('Waiting for selector of video duration');
-                await page.waitForSelector('.vjs-time-range-duration');
-
-                logger.debug('Getting the total time of the video');
-                const durationText = await page.$eval('.vjs-time-range-duration', el => {
-                    return el.innerHTML;
-                });
-                scrapingOptions.duration = this.handleDurationText(durationText);
-            }
-
-            logger.debug('Waiting for selector of fullscren button');
-            await page.waitForSelector('.vjs-fullscreen-toggle-control-button');
-
-            logger.debug('Clicking the fullscreen button');
-            await page.$eval('.vjs-fullscreen-toggle-control-button', el => {
-                return el.click();
-            });
-
-            logger.debug('Waiting for selector of play button');
-            await page.waitForSelector('.vjs-play-control');
-
-            logger.debug('Clicking play on the video');
-            await page.click('.vjs-play-control');
-
-            logger.debug(`Waiting for ${scrapingOptions.delayAfterVideoStarted}ms before starting recording`);
-            await page.waitForTimeout(scrapingOptions.delayAfterVideoStarted);
-
-            logger.debug('Staring recording');
-            const file = fs.createWriteStream(destPath);
-            const stream = await puppeteerStream.getStream(page, {
-                audio: scrapingOptions.audio,
-                video: scrapingOptions.video,
-                mimeType: scrapingOptions.mimeType,
-                audioBitsPerSecond: scrapingOptions.audioBitsPerSecond,
-                videoBitsPerSecond: scrapingOptions.videoBitsPerSecond,
-                frameSize: scrapingOptions.frameSize
-            });
-            stream.pipe(file);
-
-            logger.debug('Waiting for video to end. Duration is', scrapingOptions.duration);
-            await page.waitForTimeout(scrapingOptions.duration);
-
-            logger.debug(`Waiting for ${scrapingOptions.delayAfterVideoFinished}ms before stopping recording`);
-            await page.waitForTimeout(scrapingOptions.delayAfterVideoFinished);
-
-            logger.debug('Stopping recording');
-            await stream.destroy();
-            file.close();
-
-            logger.debug('Closing page');
-            await page.close();
-        } catch (error) {
-            throw new TumConfVideoScraperDuringScrapingError(error);
-        }
+        logger.debug('Clicking the button to access the video');
+        await page.waitForSelector('.btn-primary.submit');
+        await page.$eval('.btn-primary.submit', (button: HTMLButtonElement) => button.click());
     }
 }
